7481db1d9d76536b7b41c2634faa8acf0faa46ad
shinospp <ppshinos@gmail.com>
2022-11-12 00:36:36 +0530

    Project Added

---
+++ spring-github-demo/src/main/java/com/demo/test/SpringGithubDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGithubDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGithubDemoApplication.class, args);
+	}
+
+}
