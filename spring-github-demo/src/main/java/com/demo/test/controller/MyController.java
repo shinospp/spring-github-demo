f16deef946fa19a290e43d48837e00e9ba40d429
shinospp <ppshinos@gmail.com>
2022-11-12 00:47:44 +0530

    updated

--- spring-github-demo/src/main/java/com/demo/test/controller/MyController.java
+++ spring-github-demo/src/main/java/com/demo/test/controller/MyController.java
@@ -1,5 +1,14 @@
 package com.demo.test.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MyController {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World!!";
+	}
 
 }
