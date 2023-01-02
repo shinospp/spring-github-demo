f45f928eb7d5887c7a2523f229af8546a1da6886
shinospp <ppshinos@gmail.com>
2023-01-03 00:10:27 +0530

    description modified

--- spring-github-demo/src/main/java/com/demo/test/controller/MyController.java
+++ spring-github-demo/src/main/java/com/demo/test/controller/MyController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
 	
-	//This is hello end point
+	//This is hello user end point
 	@GetMapping("/hello")
 	public String hello() {
 		return "Welcome user";
